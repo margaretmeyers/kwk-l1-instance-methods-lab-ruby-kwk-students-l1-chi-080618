a17365bccb1bcab092466b01eebbd032485952d5
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-10 16:23:03 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+
+class Dog 
+  def bark 
+    puts "Woof!"
+  end
+  def sit 
+    puts "The Dog is sitting"
+  end
+end
+
+class Person
+  def talk
+    puts "Hello World!"
+  end 
+  def walk
+    puts "The Person is walking"
+  end 
+end
+    
